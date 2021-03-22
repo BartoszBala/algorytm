595227e538f23371001493f51b39c51f50b46e38
bartoszbala <bala.bartosz@gmail.com>
2021-03-23 00:08:29 +0100

    Initial commit

---
+++ src/main/java/AlgMain.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AlgMain {
+
+
+  public static int findMaxSumOfNonNeighborsNumber(List<Integer> lista) {
+
+    if (lista.isEmpty()) {
+      return 0;
+    }
+
+    Map<Integer, Integer> nodeValue = new HashMap<>();
+
+    nodeValue.put(1, lista.get(0));
+    if (lista.size() > 1)
+      nodeValue.put(2, lista.get(1));
+
+    for (int i = 1; i < lista.size() - 1; i++) {
+      if (!nodeValue.containsKey(i + 2) && i + 2 <= lista.size()) {
+        nodeValue.put(i + 2, lista.get(i + 1) + nodeValue.get(i));
+      } else if (i + 2 <= lista.size() && nodeValue.get(i + 2) < lista.get(i + 1) + nodeValue.get(i)) {
+        nodeValue.replace(i + 2, lista.get(i + 1) + nodeValue.get(i));
+      }
+
+      if (!nodeValue.containsKey(i + 3) && i + 3 <= lista.size()) {
+        nodeValue.put(i + 3, lista.get(i + 2) + nodeValue.get(i));
+      } else if (i + 3 <= lista.size() && nodeValue.get(i + 3) < (i + 3 < lista.size() ? lista.get(i + 2) : 0) + nodeValue.get(i)) {
+        nodeValue.replace(i + 3, (i + 3 < lista.size() ? lista.get(i + 2) : 0) + nodeValue.get(i));
+      }
+    }
+
+    return lista.size() > 1 ? Math.max(nodeValue.get(lista.size()), nodeValue.get(lista.size() - 1)) : lista.get(0);
+
+  }
+
+}
+
---
+++ src/test/java/TestAlgorithm.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestAlgorithm {
+
+
+  @Test
+  public void TestAlgorithmFindMaxSumNumberNotNeigbours(){
+
+    List<Integer> list1 = Arrays.asList(5);
+    List<Integer> list2 = Arrays.asList(5,12);
+    List<Integer> list3 = Arrays.asList(1,0);
+    List<Integer> list4 = Arrays.asList(1,1);
+    List<Integer> list5 = Arrays.asList(5,5,5);
+    List<Integer> list6 = Arrays.asList(26,9,9,13,17,8,25);
+    List<Integer> list7= Arrays.asList(2500,9,9,13,17,8,25);
+    List<Integer> list8 = Arrays.asList(5,99,5);
+    List<Integer> list9 = Arrays.asList(200,200,200,200,200);
+    List<Integer> list10 = Arrays.asList(0,0,0,0,0);
+    List<Integer> list11 = Arrays.asList(0,0,1,4,0);
+    List<Integer> list12 = Arrays.asList(500,500,1,499,500);
+    List<Integer> list13 = Arrays.asList(5,50,1,0,24,0,65,12,45);
+    List<Integer> list14 = Arrays.asList(0);
+    List<Integer> list15 = new ArrayList<>();
+
+    assertEquals(5, AlgMain.findMaxSumOfNonNeighborsNumber(list1));
+    assertEquals(12, AlgMain.findMaxSumOfNonNeighborsNumber(list2));
+    assertEquals(1, AlgMain.findMaxSumOfNonNeighborsNumber(list3));
+    assertEquals(1, AlgMain.findMaxSumOfNonNeighborsNumber(list4));
+    assertEquals(10, AlgMain.findMaxSumOfNonNeighborsNumber(list5));
+    assertEquals(77, AlgMain.findMaxSumOfNonNeighborsNumber(list6));
+    assertEquals(2551, AlgMain.findMaxSumOfNonNeighborsNumber(list7));
+    assertEquals(99, AlgMain.findMaxSumOfNonNeighborsNumber(list8));
+    assertEquals(600, AlgMain.findMaxSumOfNonNeighborsNumber(list9));
+    assertEquals(0, AlgMain.findMaxSumOfNonNeighborsNumber(list10));
+    assertEquals(4, AlgMain.findMaxSumOfNonNeighborsNumber(list11));
+    assertEquals(1001, AlgMain.findMaxSumOfNonNeighborsNumber(list12));
+    assertEquals(184, AlgMain.findMaxSumOfNonNeighborsNumber(list13));
+    assertEquals(0, AlgMain.findMaxSumOfNonNeighborsNumber(list15));
+
+
+
+  }
+
+
+
+
+}
